ec834100e5307f1ab5cc28c61ac112cfb2244085
Emilian Roman <roman.emilian@outlook.com>
2018-01-06 16:21:13 +0800

    Resolution class

---
+++ Ecran.GUI/Main/Resolution.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecran.GUI.Main
+{
+    public class Resolution
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public string Description {
+            get {
+                return $"{Width}x{Height}";
+            }
+        }
+    }
+}
