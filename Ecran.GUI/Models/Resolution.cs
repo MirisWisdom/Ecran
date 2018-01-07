44cae8c0f57325f61524181930eea3dac4ae7de7
Emilian Roman <roman.emilian@outlook.com>
2018-01-07 19:12:31 +0800

    Immutable Resolution class

--- Ecran.GUI/Models/Resolution.cs
+++ Ecran.GUI/Models/Resolution.cs
@@ -2,13 +2,31 @@
 {
     public class Resolution
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
+        readonly int width;
+        readonly int height;
+
+        public int Width {
+            get {
+                return width;
+            }
+        }
+
+        public int Height {
+            get {
+                return height;
+            }
+        }
 
         public string Description {
             get {
                 return $"{Width}x{Height}";
             }
+        }
+
+        public Resolution(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
         }
     }
 }
