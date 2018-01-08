5b99be1424c74a9a50acd81796733ec5ea23efae
Emilian Roman <roman.emilian@outlook.com>
2018-01-08 11:14:55 +0800

    Bump to 0.2.2

--- Ecran.GUI/Properties/Resources.Designer.cs
+++ Ecran.GUI/Properties/Resources.Designer.cs
@@ -133,7 +133,7 @@ namespace Ecran.GUI.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.2.1.
+        ///   Looks up a localized string similar to 0.2.2.
         /// </summary>
         public static string Version {
             get {
